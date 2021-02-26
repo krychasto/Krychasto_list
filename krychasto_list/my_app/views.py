dda75d11c077691f6f04abfa6d9da1cddd4e5bbb
Krystian Freus <krystian.freus@wp.pl>
2021-02-26 14:57:32 +0100

    Ending frontend and search function

--- krychasto_list/my_app/views.py
+++ krychasto_list/my_app/views.py
@@ -1,15 +1,39 @@
 import requests
-from django.shortcuts import render
 from bs4 import BeautifulSoup
+from django.shortcuts import render
+from requests.compat import quote_plus
+from . import models
 
-# Create your views here.
+BASE_CRAIGSLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
+
 def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = BASE_CRAIGSLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup = BeautifulSoup(data, features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = 'N/A'
+
+        final_postings.append((post_title, post_url, post_price))
+
     stuff_for_frontend = {
-        'search' : search,
+        'search': search,
+        'final_postings': final_postings,
     }
+
     return(render(request, 'my_app/new_search.html', stuff_for_frontend))
