639f67738363be3280030b22c60b484eac63fca2
Krystian Freus <krystian.freus@wp.pl>
2021-02-26 11:45:02 +0100

    Frontend p2

--- krychasto_list/my_app/views.py
+++ krychasto_list/my_app/views.py
@@ -5,4 +5,9 @@ def home(request):
     return render(request, 'base.html')
 
 def new_search(request):
-    return(render(request, 'my_app/new_search.html'))
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search' : search,
+    }
+    return(render(request, 'my_app/new_search.html', stuff_for_frontend))
